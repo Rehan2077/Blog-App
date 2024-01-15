14cef436777b6a6010cbaf665c07ffb41eaa6a04
Rehan2077 <rfazal0124@gmail.com>
2024-01-16 00:20:32 +0530

    minor bug fix

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -26,12 +26,17 @@ const Article = () => {
     scrollToTop();
   });
 
+  if (isError) {
+    return null;
+  }
+
   return (
     <section className="container mx-auto flex flex-wrap p-5 md:gap-5">
       <div className="flex flex-wrap justify-center gap-x-7 gap-y-10 md:gap-x-10 xl:gap-x-7 2xl:justify-normal 2xl:gap-x-11">
-        {isLoading && isError
-          ? [...Array(4)].map((_, i) => <ArticleCardSkeleton key={i} />)
-          : data?.data?.posts.length>0 && data?.data?.posts.map((post) => (
+        {isLoading
+          ? Array.from({ length: 4 }, (_, i) => <ArticleCardSkeleton key={i} />)
+          : data?.data?.posts.length > 0 &&
+            data?.data?.posts.map((post) => (
               <ArticleCard key={post._id} post={post} />
             ))}
       </div>
